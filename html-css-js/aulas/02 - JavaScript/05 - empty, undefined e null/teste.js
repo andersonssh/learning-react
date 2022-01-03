e87a21ef966268dd121a12c4f0c7b22d853c5940
andersonssh <4nd3r50n4r4uj0@gmail.com>
2022-01-03 13:05:38 -0300

    Adiciona testes undefined, null e empty

---
+++ html-css-js/aulas/02 - JavaScript/05 - empty, undefined e null/teste.js
@@ -0,0 +1,19 @@
+// empty: quando o numero é 0. a string é vazia. array foi apenas inicializado.
+// sem valor!
+
+// null: propositalmente o valor nao existe
+
+// undefined:  valor nao existe. nao foi definido
+
+console.log(typeof(naoDefinido)) // undefined
+
+// let naoDefinido -> erro de undefined
+
+let nulo = null
+
+console.log(0 == false) // true
+console.log([] == false) // true
+console.log('' == false) // true
+console.log(null == false) // FALSEEE
+console.log(undefined == false) // FALSE
+console.log(nulo === false) // false
