98a3c5b327a72865fab0e7a8e590626caca11c61
andersonssh <4nd3r50n4r4uj0@gmail.com>
2022-01-03 12:39:06 -0300

    Adiciona anotacoes de funcoes para vet

---
+++ html-css-js/aulas/02 - JavaScript/03 - arrays/vetores.js
@@ -0,0 +1,9 @@
+let a = [1,2,3]
+a.push(2) //adiciona no final
+console.log(a) // [1, 2, 3, 2]
+a.pop() // tira do ultimo
+console.log(a) // [1, 2, 3]
+a.shift() // tira do inicio
+console.log(a) // [2, 3]
+a.unshift(2) // adiciona no inicio
+console.log(a) // [2, 2 ,3]
