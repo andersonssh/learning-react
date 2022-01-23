5552d9998649d50fe819dffa5488898703907642
andersonssh <4nd3r50n4r4uj0@gmail.com>
2022-01-23 10:06:37 -0300

    Adiciona testes de useState

--- react/02 - parte 2/projeto2/src/components/Form.js
+++ react/02 - parte 2/projeto2/src/components/Form.js
@@ -1,18 +1,31 @@
+import { useState } from 'react'
+
 function Form(){
     function cadastrarUsuario(e){
         // para html no envio de requisicao pro server e passa o comando para o corpo desta funcao
         e.preventDefault()
+        console.log(name, password)
         console.log('script para cadastro de usuario')
     }
+
+    const [name, setName] = useState('Nome padrao')
+    const [password, setPassword] = useState()
+
     return (  
         <div>
             <h1>Cadastro</h1>
             <form action="" onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="digite seu nome" />
+                    <label htmlFor="name"></label>
+                    <input onChange={ (e) => setName(e.target.value) } value={name} name="name" type="text" placeholder="digite seu nome" />
                 </div>
                 <div>
-                    <input type="submit" value="Cadastrar" />
+                    <label htmlFor="password"></label>
+                    <input onChange={(e) => setPassword(e.target.value)} name="password" type="password" placeholder="digite a sua senha" />
+                </div>
+                <div>
+                    
+                    <input type="submit" name="password" value="Cadastrar" />
                 </div>
             </form>
         </div>
