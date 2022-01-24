f9d8bffebfe183ff1daa5adc71e1641b962fd8b3
andersonssh <4nd3r50n4r4uj0@gmail.com>
2022-01-23 22:54:53 -0300

    Adiciona testes de renderizacao de listas

--- react/02 - parte 2/projeto2/src/App.js
+++ react/02 - parte 2/projeto2/src/App.js
@@ -3,14 +3,37 @@ import './App.css';
 import Evento from './components/Evento';
 import Form from './components/Form'
 import Condicional from './components/Condicional';
+import Listas from './components/Listas';
 
 function App() {
+  const x = [{
+    nome: 'juse', 
+    idade: 12
+  },{
+    nome: 'jusesao2',
+    idade: 13
+  },{
+    nome: 'pedro2',
+    idade: 16
+  },{
+    nome: 'pedrao',
+    idade: 23
+  }]
+  const y = [
+    'joao',
+    'pedro',
+    'jose'
+  ]
   return (
    <div className="App">
      <Evento numero="1"/>
      <Form />
      <h1>Condicional</h1>
      <Condicional />
+     <h1>renderizacao de listas</h1>
+     <Listas itens={y} lista={x}/>
+     <Listas itens={[]} lista={x}/>
+     
    </div>
   );
 }
